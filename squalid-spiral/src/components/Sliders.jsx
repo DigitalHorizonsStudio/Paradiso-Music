e41dc3abea97d7d7835afbbd8e38fdf2a2eaf5e3
ThiagoCancellarich <131214817+ThiagoCancellarich@users.noreply.github.com>
2024-03-15 21:38:47 -0300

    carrusel

---
+++ squalid-spiral/src/components/Sliders.jsx
@@ -0,0 +1,40 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import React from "react";
+import Slider from "react-slick";
+
+import foto1 from '../Assets/ParadisoMusicLogo.png'
+
+
+export default function SimpleSlider() {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  return (
+    <Slider {...settings}>
+      <div>
+        <img class='w-40 h-40' src={foto1.src} alt="foto" />
+      </div>
+      <div>
+      <h3>2</h3>
+      </div>
+      <div>
+      <h3>3</h3>
+      </div>
+      <div>
+        <h3>4</h3>
+      </div>
+      <div>
+        <h3>5</h3>
+      </div>
+      <div>
+        <h3>6</h3>
+      </div>
+    </Slider>
+  );
+}
